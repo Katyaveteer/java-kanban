99f20d81e8cfc9d945e0fc693a32d8a6bdd458ee
Katya <katyaveteer@yandex.ru>
2025-04-22 16:32:41 +0300

    исправлен CodeStyle

--- src/com/yandex/app/model/Subtask.java
+++ src/com/yandex/app/model/Subtask.java
@@ -7,7 +7,7 @@ public class Subtask extends Task {
         super(id, title, description, status);
         this.epicId = epicId;
     }
-    public  Subtask (String title, String description,int epicId) {
+    public  Subtask(String title, String description,int epicId) {
         super(title,description);
         this.epicId = epicId;
     }
