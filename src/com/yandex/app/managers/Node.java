b36eae1eb229c1471229c9e2e3766852983c4c94
Katya <katyaveteer@yandex.ru>
2025-04-22 16:19:16 +0300

    добавлен класс

---
+++ src/com/yandex/app/managers/Node.java
@@ -0,0 +1,15 @@
+package com.yandex.app.managers;
+
+import com.yandex.app.model.Task;
+
+class Node {
+     Node next;
+     Node prev;
+     Task task;
+
+    public  Node(Task task, Node prev, Node next) {
+        this.next = next;
+        this.prev = prev;
+        this.task = task;
+    }
+}
